55597dc9682fe4782bb0ef0fa5f4500a56c755a5
Jorge Gu7man <46007655+jgu7man@users.noreply.github.com>
2023-02-27 01:50:10 -0600

    💥 feat(forms): mx-forms library created

---
+++ src/public-api.ts
@@ -0,0 +1,11 @@
+/*
+ * Public API Surface of forms
+ */
+
+export * from './lib/form-fields/default-field/default-field.component';
+export * from './lib/form-fields/field.model';
+export * from './lib/form-fields/fields.module';
+export * from './lib/forms.component';
+export * from './lib/forms.module';
+export * from './lib/forms.service';
+
